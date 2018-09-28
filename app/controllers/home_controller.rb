4b84317645ed9eb1e153228a282c53fe765c69b9
cfaloon <cfaloon@gmail.com>
2018-09-27 23:55:05 -0400

    Adds headers to request parameters in Home#test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,14 @@ class HomeController < ApplicationController
   end
 
   def test
-    header = {"Accept": "application/json", 
-    	"app_id": "#{ENV['oed_app_id']}", 
-    	"app_key": "#{ENV['oed_app_key']}"}
-    @response = HTTParty.get("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace", header)
+    parameters = {
+      headers: {
+        "Accept": "application/json",
+        "app_id": "#{ENV['oed_app_id']}",
+        "app_key": "#{ENV['oed_app_key']}",
+        }
+      }
+    @response = HTTParty.get("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace", parameters)
     # @location = response['name']
   end
 end
